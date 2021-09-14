02700f97a35cd75bc80e4d77b3538be791c6ea6f
Devan Andersen <devan.andersen@shopify.com>
2021-09-14 16:24:17 -0400

    Better naming, fix bug with printing

--- ProofOfConcept/website-compiler/src/main.rs
+++ ProofOfConcept/website-compiler/src/main.rs
@@ -1,9 +1,9 @@
-mod website_compiler;
+mod website_fetcher;
 
 #[tokio::main]
 async fn main() {
-    let website_one = website_compiler::get_website("https://facebook.com").await;
-    println!("{}", website_one);
-    let website_two = website_compiler::get_website("https://google.com").await;
-    println!("{}", website_two);
+    let website_one = website_fetcher::get_website("https://facebook.com").await;
+    println!("{:?}", website_one);
+    let website_two = website_fetcher::get_website("https://google.com").await;
+    println!("{:?}", website_two);
 }
