ea748ae2993f9761032ed99fa15cbdb333705ee5
Devan Andersen <devan.andersen@shopify.com>
2021-11-20 17:59:19 -0500

    Add code for multiple helper websites

--- ProofOfConcept/website-compiler/src/main.rs
+++ ProofOfConcept/website-compiler/src/main.rs
@@ -8,13 +8,17 @@ use std::env;
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let candidate_website_url = "https://carleton.ca";
-    let helper_website_url = "https://carleton.ca";
+    let helper_website_url = "https://www2.uottawa.ca";
+    let second_helper_website_url = "https://nytimes.com";
     let sequence_length = 5;
 
+    let mut helper_websites = Vec::new();
     let mut locations_list = resource_manager::get_byte_sequence_locations_list();
-
     let mut candidate_website = resource_manager::get_website(candidate_website_url).await.unwrap();
-    let mut helper_website = resource_manager::get_website(helper_website_url).await.unwrap();
+    let helper_website = resource_manager::get_website(helper_website_url).await.unwrap();
+    let second_helper_website = resource_manager::get_website(second_helper_website_url).await.unwrap();
+    helper_websites.push(helper_website.clone());
+    helper_websites.push(second_helper_website.clone());
 
     if args.contains(&String::from("bench")) {
         println!("Running Benchmark Timing...\n---------------------------------------");
@@ -26,16 +30,16 @@ async fn main() {
 
     println!("Running Sequence Algorithm...\n---------------------------------------");
     let compile_time_start = Instant::now();
-    website_compiler::compute_matching_sequences(&mut candidate_website, &mut helper_website, &mut locations_list, sequence_length);
+    website_compiler::compute_matching_sequences(&mut candidate_website, &mut helper_websites, &mut locations_list, sequence_length);
     let compile_time_end = compile_time_start.elapsed();
     println!("Compilation execution time:\n\t- Seconds: {}\n\t- Milliseconds {}\n", compile_time_end.as_secs(), compile_time_end.as_millis());
 
     println!("Constructing Website from Mapping...\n---------------------------------------");
-    let compiled_candidate_website = website_compiler::compile_decentralized_source(&mut helper_website, &mut locations_list);
+    let compiled_candidate_website = website_compiler::compile_decentralized_source(&mut helper_websites, &mut locations_list);
     resource_manager::store_website_file(candidate_website_url, &candidate_website);
     resource_manager::store_website_file(helper_website_url, &helper_website);
     resource_manager::store_website_file("https://recompiled_website.com", &compiled_candidate_website);
     //resource_manager::_store_locations_list(locations_list);
-    benchmark_functions::final_stats_and_proportions(candidate_website, helper_website, sequence_length);
+    benchmark_functions::final_stats_and_proportions(candidate_website, helper_websites, sequence_length);
     println!("\nCompilation complete - compiled website stored at ./website_store/recompiled_website.com.html");
 }
--- ProofOfConcept/website-compiler/src/website_compiler.rs
+++ ProofOfConcept/website-compiler/src/website_compiler.rs
@@ -1,86 +1,101 @@
 use serde_json::Value;
 use regex::Regex;
 
-pub fn compute_matching_sequences(candidate_website: &mut String, helper_website: &mut String, locations_list: &mut serde_json::Map<String, Value>, sequence_length: usize) {
+pub fn compute_matching_sequences(candidate_website: &mut String, helper_websites: &mut Vec<String>, locations_list: &mut serde_json::Map<String, Value>, sequence_length: usize) {
     let comment_regex = Regex::new(r"<!--(.*?)-->").unwrap();
     *candidate_website = comment_regex.replace_all(candidate_website, "").trim().to_string();
-    *helper_website = comment_regex.replace_all(helper_website, "").trim().to_string();
-    helper_website.push_str("\n");
+    let mut insertion_strings = Vec::new();
+    for helper_website in helper_websites.iter_mut() {
+        let new_helper_website = comment_regex.replace_all(helper_website, "").trim().to_string();
+        *helper_website = new_helper_website;
+        helper_website.push_str("\n");
 
-    let mut insertion_string = String::from("");
+        insertion_strings.push(String::from(""));
+    }
+
     let mut curr_sequence_length = sequence_length;
     let compile_website_length = candidate_website.chars().count();
 
     while curr_sequence_length > 0 {
         let candidate_website_chars = candidate_website.chars();
-        let helper_website_chars = helper_website.chars();
         let candidate_website_tuple = gather_char_sequences(candidate_website_chars, curr_sequence_length);
         let candidate_website_sequences = candidate_website_tuple.0;
         let candidate_website_indexes = candidate_website_tuple.1;
-        let helper_website_tuple = gather_char_sequences(helper_website_chars, curr_sequence_length);
-        let helper_website_sequences = helper_website_tuple.0;
-        let helper_website_indexes = helper_website_tuple.1;
 
-        'outer: for (_index_one, sequence_one) in candidate_website_sequences.iter().enumerate() {
-            let mut sequence_included = true;
-            for index_accounted_for in candidate_website_indexes[_index_one][0]..candidate_website_indexes[_index_one][1] {
-                if !locations_list.contains_key(&index_accounted_for.to_string()) {
-                    sequence_included = false;
+        for (index, helper_website) in helper_websites.iter().enumerate() {
+            let helper_website_chars = helper_website.chars();
+            let helper_website_tuple = gather_char_sequences(helper_website_chars, curr_sequence_length);
+            let helper_website_sequences = helper_website_tuple.0;
+            let helper_website_indexes = helper_website_tuple.1;
+
+            'outer: for (_index_one, sequence_one) in candidate_website_sequences.iter().enumerate() {
+                let mut sequence_included = true;
+                for index_accounted_for in candidate_website_indexes[_index_one][0]..candidate_website_indexes[_index_one][1] {
+                    if !locations_list.contains_key(&index_accounted_for.to_string()) {
+                        sequence_included = false;
+                    }
                 }
-            }
-            if sequence_included { continue 'outer; }
-            for (_index_two, sequence_two) in helper_website_sequences.iter().enumerate() {
-                if sequence_one == sequence_two
-                    && candidate_website_indexes[_index_one][0] <= compile_website_length {  
-                        insertion_string.push_str(&format!("{}-{}:{},", helper_website_indexes[_index_two][0], (helper_website_indexes[_index_two][1]), candidate_website_indexes[_index_one][0]));
-                        for index_accounted_for in candidate_website_indexes[_index_one][0]..candidate_website_indexes[_index_one][1] {
-                            locations_list.insert(index_accounted_for.to_string(), Value::String(sequence_one.to_string()));
-                        }
-                        continue 'outer;
+                if sequence_included { continue 'outer; }
+                for (_index_two, sequence_two) in helper_website_sequences.iter().enumerate() {
+                    if sequence_one == sequence_two
+                        && candidate_website_indexes[_index_one][0] <= compile_website_length {
+                            insertion_strings[index].push_str(&format!("{}-{}:{},", helper_website_indexes[_index_two][0], (helper_website_indexes[_index_two][1]), candidate_website_indexes[_index_one][0]));
+                            for index_accounted_for in candidate_website_indexes[_index_one][0]..candidate_website_indexes[_index_one][1] {
+                                locations_list.insert(index_accounted_for.to_string(), Value::String(sequence_one.to_string()));
+                            }
+                            continue 'outer;
+                    }
                 }
             }
         }
         curr_sequence_length -= 1;
     }
 
-    insertion_string.pop();
-    insertion_string.insert_str(0, "<!--");
-    insertion_string.push_str("-->");
 
-    helper_website.push_str(&insertion_string);
+    for (index, helper_website) in helper_websites.iter_mut().enumerate() {
+        insertion_strings[index].pop();
+        insertion_strings[index].insert_str(0, "<!--");
+        insertion_strings[index].push_str("-->");
+        helper_website.push_str(&insertion_strings[index]);
+    }
 }
 
-pub fn compile_decentralized_source(helper_website: &mut String, _locations_list: &mut serde_json::Map<String, Value>) -> String {
+pub fn compile_decentralized_source(helper_websites: &mut Vec<String>, _locations_list: &mut serde_json::Map<String, Value>) -> String {
     let comment_regex = Regex::new(r"<!--(.*?)-->").unwrap();
-    let comment_list: Vec<Vec<_>> = comment_regex.captures_iter(helper_website)
-        .map(|c| c.iter().map(|m| m.unwrap().as_str()).collect())
-        .collect();
-    let comment_for_compiling: Vec<&str> = comment_list.last().unwrap().last().unwrap().split(",").collect();
     let mut new_compiled_website_string = String::from("");
 
-    for sequence_set in comment_for_compiling {
-        let sequence_mappings: Vec<&str> = sequence_set.split(":").collect();
-        let sequence_origin_location: Vec<usize> = sequence_mappings.first().unwrap().split("-")
-            .map(|x| x.parse::<usize>().unwrap())
+    for helper_website in helper_websites.iter_mut() {
+        let comment_list: Vec<Vec<_>> = comment_regex.captures_iter(helper_website)
+            .map(|c| c.iter().map(|m| m.unwrap().as_str()).collect())
             .collect();
-        let beginning_char: usize = *sequence_origin_location.first().unwrap();
-        let ending_char: usize = *sequence_origin_location.last().unwrap();
-        let length = ending_char-beginning_char;
-        let placement_location: usize = sequence_mappings.last().unwrap().parse::<usize>().unwrap();
+        let comment_for_compiling: Vec<&str> = comment_list.last().unwrap().last().unwrap().split(",").collect();
 
-        let helper_website_str = helper_website.as_str();
-        if new_compiled_website_string.len() <= placement_location+length {
-            for _ in new_compiled_website_string.len()..placement_location+length {
-                new_compiled_website_string.push(' ');
+        if comment_for_compiling != [""] {
+            for sequence_set in comment_for_compiling {
+                let sequence_mappings: Vec<&str> = sequence_set.split(":").collect();
+                let sequence_origin_location: Vec<usize> = sequence_mappings.first().unwrap().split("-")
+                    .map(|x| x.parse::<usize>().unwrap())
+                    .collect();
+                let beginning_char: usize = *sequence_origin_location.first().unwrap();
+                let ending_char: usize = *sequence_origin_location.last().unwrap();
+                let length = ending_char-beginning_char;
+                let placement_location: usize = sequence_mappings.last().unwrap().parse::<usize>().unwrap();
+
+                let helper_website_str = helper_website.as_str();
+                if new_compiled_website_string.len() <= placement_location+length {
+                    for _ in new_compiled_website_string.len()..placement_location+length {
+                        new_compiled_website_string.push(' ');
+                    }
+                }
+
+
+                let sequence_to_print = &helper_website_str[beginning_char..ending_char];
+                if new_compiled_website_string.len() == placement_location {
+                    new_compiled_website_string.push_str(&sequence_to_print);
+                } else {
+                    new_compiled_website_string.replace_range(placement_location..placement_location+length, &sequence_to_print);
+                }
             }
-        }
-
-
-        let sequence_to_print = &helper_website_str[beginning_char..ending_char];
-        if new_compiled_website_string.len() == placement_location {
-            new_compiled_website_string.push_str(&sequence_to_print);
-        } else {
-            new_compiled_website_string.replace_range(placement_location..placement_location+length, &sequence_to_print);
         }
     }
 
